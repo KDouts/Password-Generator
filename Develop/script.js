6467ba703c6d1ab0ecaa3d987feb575e5ae94db0
Kurtis Doute <kcd257@yahoo.com>
2021-11-17 22:46:02 -0800

    planning for js

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = "";
 
-const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
-const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
 
+
+var lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
+var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
+
+console.log(upperAlpha);
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var length = prompt("How long is your password?", "maximun allowed: 128");
+  var hasUpper = prompt("Does your password contain Uppercase characters?", "Yes or No");
+  var hasLower = prompt("Does your password contain Lowercase characters?", "Yes or No");
+  var hasSpecial = prompt("Does your password contain Special characters?", "Yes or No");
+  var hasNumber = prompt("Does your password contain numbers?", "Yes or No");
+  
+  var password = generatePassword(length, hasUpper, hasLower, hasSpecial, hasNumber);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+function generatePassword(length, hasUpper, hasLower, hasSpecial, hasNumber) {
+  var characterBank = "";
+  if (length > 128){
+    length = 128;
+  }
+  console.log(length, hasUpper, hasLower, hasSpecial, hasNumber);
+
+  //if has-blank- then add string to characterBank
+
+  // loop over randomizer x times where x = length
+
+  //  create ranom number from 0 - characterBank.length
+
+  // push characterBank[randomnumber] to password
+
+  
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
