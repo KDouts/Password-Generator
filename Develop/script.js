387855ae5f79b7ff5bd52ed7e2ba05bbce1fcb5d
Kurtis Doute <kcd257@yahoo.com>
2021-11-24 18:34:12 -0800

    Rearranging if functions to work with the code better and added alert when the password is not the appropriate length

--- Develop/script.js
+++ Develop/script.js
@@ -9,15 +9,45 @@ var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*_-+=";
 
+var characterBank = [];
+
 console.log(upperAlpha);
 // Write password to the #password input
 function writePassword() {
   var length = prompt("How long is your password?", "8-128");
+  console.log(length);
+  if (length > 128 || length < 8){
+    
+    alert( length + " is not an appropriate length.");
+  
+    writePassword();
+  
+  }
+
   var hasUpper = confirm("Does your password contain Uppercase characters?");
+
+  if (hasUpper === true) {
+    characterBank += upperAlpha
+  }
+
   var hasLower = confirm("Does your password contain Lowercase characters?");
+
+  if (hasLower === true) {
+    characterBank += lowerAlpha
+  }
+
   var hasSpecial = confirm("Does your password contain Special characters?",);
+
+  if (hasSpecial === true) {
+    characterBank += symbols
+  }
+
   var hasNumber = confirm("Does your password contain numbers?",);
   
+  if (hasNumber === true) {
+    characterBank += numbers
+  }
+
   var password = generatePassword(length, hasUpper, hasLower, hasSpecial, hasNumber);
   console.log(password);
   var passwordText = document.querySelector("#password");
@@ -27,33 +57,8 @@ function writePassword() {
 }
 
 function generatePassword(length, hasUpper, hasLower, hasSpecial, hasNumber) {
-  var characterBank = [];
   var password = "";
-  if (length > 128){
-    // if (null === length) return false;
-    length = 128;
-    // alert( length ' is not a valid response');
-    // return;
-  }
   console.log(length, hasUpper, hasLower, hasSpecial, hasNumber);
-
-  //if has-blank- then add string to characterBank
-  if (hasUpper === true) {
-    characterBank += upperAlpha
-  }
-
-  if (hasLower === true) {
-    characterBank += lowerAlpha
-  }
-
-  if (hasSpecial === true) {
-    characterBank += symbols
-  }
-
-  if (hasNumber === true) {
-    characterBank += numbers
-  }
-
   for ( let i = 0; i < length; i++) {
     password += characterBank[Math.floor(Math.random() * characterBank.length)];
   }
